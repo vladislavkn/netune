1740f8f380b9250df04317ef0c090e81fc2d8214
Vladislav Knyshov <inova136@gmail.com>
2024-03-16 18:04:38 +0100

    fix: shharing text

--- src/lib/share.ts
+++ src/lib/share.ts
@@ -1,11 +1,12 @@
 import { toast } from "sonner";
 
 const share = async (title: string, text: string) => {
+  text = `${title}\n${text}`;
   try {
     if (navigator.canShare({ title, text, url: location.href })) {
       await navigator.share({ title, text, url: location.href });
     } else {
-      await navigator.clipboard.writeText(`${title}\n${text}`);
+      await navigator.clipboard.writeText(text);
       toast("Can't share, so copied to clipboard!");
     }
   } catch {
