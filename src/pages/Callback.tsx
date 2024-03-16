745d410bb9205a540bba1ddbbe5042be01e49b2a
Vladislav Knyshov <inova136@gmail.com>
2024-03-16 18:29:57 +0100

    feat: update styles on callback page

--- src/pages/Callback.tsx
+++ src/pages/Callback.tsx
@@ -1,6 +1,7 @@
 import { FC, useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import SpotifyApi from "../spotify/spotifyApi";
+import { Button } from "@/components/ui/button";
 
 const Callback: FC = () => {
   const [error, setError] = useState<string | undefined>();
@@ -22,9 +23,11 @@ const Callback: FC = () => {
   if (error) {
     return (
       <div>
-        <h1>Authorization failed</h1>
-        <p>{error}</p>
-        <Link to="/">Return to login</Link>
+        <h1 className="text-xl mb-2">Authorization failed</h1>
+        <p className="text-gray-600 mb-4">{error}</p>
+        <Link to="/">
+          <Button>Return to login</Button>
+        </Link>
       </div>
     );
   }
