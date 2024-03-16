4f8f755c51382aad1bccd810bf8a66ba34c595b8
Vladislav Knyshov <inova136@gmail.com>
2024-03-16 17:54:34 +0100

    fix: sharing

--- src/lib/share.ts
+++ src/lib/share.ts
@@ -2,8 +2,8 @@ import { toast } from "sonner";
 
 const share = async (title: string, text: string) => {
   try {
-    if (navigator.share) {
-      await navigator.share({ title, text });
+    if (navigator.canShare({ title, text, url: location.href })) {
+      await navigator.share({ title, text, url: location.href });
     } else {
       await navigator.clipboard.writeText(`${title}\n${text}`);
       toast("Can't share, so copied to clipboard!");
