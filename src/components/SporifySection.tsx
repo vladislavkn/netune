4aed44876843a2edb4d2d27cb09d201e64588fc3
Vladislav Knyshov <inova136@gmail.com>
2024-03-16 16:46:55 +0100

    update: styles

--- src/components/SporifySection.tsx
+++ src/components/SporifySection.tsx
@@ -12,8 +12,8 @@ function SpotifySection<T extends { id: string }>({
   itemComponent: Item,
 }: SpotifySectionProps<T>) {
   return (
-    <section className="rounded-md bg-zinc-900 text-white p-3">
-      <h2 className="text-2xl font-bold px-3 mb-2">{title}</h2>
+    <section className="rounded-md bg-zinc-900 text-white px-1 py-2 sm:px-3 sm:py-3">
+      <h2 className="text-2xl font-bold px-3 py-2">{title}</h2>
       <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         {items.map((item) => (
           <Item key={item.id} {...item} />
