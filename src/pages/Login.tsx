eaeb04e573843d04386fa0afb7e347637f6a66dd
Vladislav Knyshov <inova136@gmail.com>
2024-03-16 10:30:42 +0100

    feat: style main page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,18 @@
 import { FC } from "react";
 import openAuthorisationPage from "../spotify/openAuthorisationPage";
+import { Button } from "@/components/ui/button";
+import { LogIn as LoginIcon } from "lucide-react";
 
 const Login: FC = () => {
   return (
-    <div>
-      <button onClick={openAuthorisationPage}>Login with spotify</button>
+    <div className="container max-w-[38rem] mx-auto mt-24 flex flex-col items-stretch">
+      <h2 className="font-bold text-6xl">Use the power of AI</h2>
+      <h3 className="font-medium text-6xl text-gray-400 mb-8">
+        to find new tracks to listen to.
+      </h3>
+      <Button onClick={openAuthorisationPage}>
+        <LoginIcon className="w-4 h-4 mr-2" /> Login with spotify
+      </Button>
     </div>
   );
 };
